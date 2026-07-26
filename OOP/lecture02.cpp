ad304629cfee8b34691343ee57637f84e37f3da3
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-07-26 12:52:11 +0500

    copy constructor

--- OOP/lecture02.cpp
+++ OOP/lecture02.cpp
@@ -38,23 +38,23 @@
 // }
 
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-class Customer {
-    string name;
-    int acc_num;
-    int balance;
+// class Customer {
+//     string name;
+//     int acc_num;
+//     int balance;
 
 
-    public:
+//     public:
 
-    //default constructor
-    Customer(){
-        name= "Huzaifa";
-        acc_num= 123;
-        balance= 1000;
-    }
+//     //default constructor
+//     Customer(){
+//         name= "Huzaifa";
+//         acc_num= 123;
+//         balance= 1000;
+//     }
 
     // parameterized constructor with 3 arguments
     // Customer(string name, int acc_num, int balance){
@@ -65,27 +65,59 @@ class Customer {
 
 
     //inline constructor
-    inline Customer(string a, int b, int c): name(a), acc_num(b), balance(c){
+//     inline Customer(string a, int b, int c): name(a), acc_num(b), balance(c){
 
+//     }
+
+//     //parameterized constructor with 2 arguments
+//     Customer(string name, int acc_num){
+//         this-> name= name;
+//         this-> acc_num= acc_num;
+//         balance= 100;
+//     }
+
+//     void display(){
+//         cout<<"Name: "<<name<<"\nAccount Number: "<<acc_num<<"\nBalance: "<<balance<<endl<<"-----\n";
+//     }
+// };
+
+// int main (){
+//     Customer A1;
+//     Customer A2("Ali", 123, 1000);
+//     Customer A3("Zubair", 123);
+//     A1.display();
+//     A2.display();
+//     A3.display();
+// }
+
+
+//copy contructor
+
+#include<iostream>
+using namespace std;
+
+class Customer {
+    private:
+    string name;
+    int balance;
+    public:
+    Customer(string a, int b){
+        name = a;
+        balance = b;
     }
-
-    //parameterized constructor with 2 arguments
-    Customer(string name, int acc_num){
-        this-> name= name;
-        this-> acc_num= acc_num;
-        balance= 100;
+    Customer(Customer &B){
+        name = B.name;
+        balance= B.balance;
     }
-
     void display(){
-        cout<<"Name: "<<name<<"\nAccount Number: "<<acc_num<<"\nBalance: "<<balance<<endl<<"-----\n";
+        cout<<"\nName: "<<name<<"\nBalance: "<<balance<<"\n-------------"<<endl;
     }
 };
 
 int main (){
-    Customer A1;
-    Customer A2("Ali", 123, 1000);
-    Customer A3("Zubair", 123);
-    A1.display();
-    A2.display();
-    A3.display();
+    Customer C1("Huzaifa", 1234);
+    C1.display();
+    Customer C2(C1);
+    C2.display();
+    
 }
