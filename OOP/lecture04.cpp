e6bfcde35ea32c59f9692c72d843aeb80b44689b
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-07-30 15:15:27 +0500

    inhertitance

--- OOP/lecture04.cpp
+++ OOP/lecture04.cpp
@@ -1,29 +1,91 @@
 // inheritance
 
 // private, public, protected
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// class Human
+// {
+// private:
+//     int a;
+
+// protected:
+//     int c;
+
+// public:
+//     int b;
+//     void fun()
+//     {
+//         a = 2, b = 4, c = 5;
+//         cout << a << " " << b << " " << c;
+//     }
+// };
+
+// int main()
+// {
+//     Human Huzaifa;
+//     Huzaifa.b;
+//     Huzaifa.fun();
+// }
+
+
+
+//parent and child class
+
+// #include<iostream>
+// using namespace std;
+
+// class Human{
+
+//     public:
+//     string name;
+//     int age;
+
+// };
+
+// class Student : private Human{
+
+//     private:
+//     int marks;
+
+// };
+
+// int main (){
+//     Student A;
+//     A.name = "Huzaifa";
+// }
+
+
+#include<iostream>
 using namespace std;
 
 class Human
 {
-private:
-    int a;
+    protected:
+    string name;
+    int age;  
+};
 
-protected:
-    int c;
-
-public:
-    int b;
-    void fun()
+class Student : protected Human 
+{
+    private: 
+    float marks;
+    public:
+    void addInfo(string name, int age, float marks)
     {
-        a = 2, b = 4, c = 5;
-        cout << a << " " << b << " " << c;
+        this ->name = name;
+        this ->age = age;
+        this ->marks = marks;
+    }
+    void display()
+    {
+        cout<<name<<" "<<age<<" "<<marks<<endl; 
     }
 };
 
-int main()
-{
-    Human Huzaifa;
-    Huzaifa.b;
-    Huzaifa.fun();
+int main (){
+    Student s1;
+    s1.addInfo("Huzaifa", 21, 3.38);
+    s1.display();
+    return 0;
 }
