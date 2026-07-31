0bbdc01168997130c1d6acfc60678eb6b2923578
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-07-31 12:57:33 +0500

    hieararchical inheritance

--- OOP/lecture05.cpp
+++ OOP/lecture05.cpp
@@ -1,56 +1,112 @@
-// // types of inheritance
+// // // types of inheritance
 
-// // single level inheritance
+// // // single level inheritance
 
-// // #include <iostream>
+// // // #include <iostream>
+// // // using namespace std;
+
+// // // class Human
+// // // {
+// // // protected:
+// // //     string name;
+// // //     int age;
+
+// // // public:
+// // //     void work()
+// // //     {
+// // //         cout << "Human is working\n";
+// // //     }
+// // //     void display()
+// // //     {
+// // //         cout<<name<<" "<<age<<endl;
+// // //     }
+// // // };
+
+// // // class Student : public Human
+// // // {
+// // // private:
+// // //     int roll_number, fees;
+
+// // // public:
+// // //     Student(string name, int age, int roll_number, int fees)
+// // //     {
+// // //         this->name = name;
+// // //         this->age = age;
+// // //         this->roll_number = roll_number;
+// // //         this->fees = fees;
+// // //     }
+// // //     // void display ()
+// // //     // {
+// // //     //     cout<<"Name: "<<name<<"\nage: "<<age<<"\nRoll No: "<<roll_number<<"\nFees: "<<fees<<endl;
+// // //     // }
+// // // };
+
+// // // int main ()
+// // // {
+// // //     Student s("Huzaifa", 21, 1882, 40000);
+// // //     s.display();
+// // //     // s.work();
+// // //     return 0;
+// // // }
+
+
+// // //multilevel inheritance
+
+// // #include<iostream>
 // // using namespace std;
 
-// // class Human
+// // class Person 
 // // {
-// // protected:
+// //     protected:
 // //     string name;
-// //     int age;
-
-// // public:
-// //     void work()
+// //     public:
+// //     void introduce()
 // //     {
-// //         cout << "Human is working\n";
-// //     }
-// //     void display()
-// //     {
-// //         cout<<name<<" "<<age<<endl;
+// //         cout<<"My name is: "<<name<<endl;
 // //     }
 // // };
 
-// // class Student : public Human
+// // class Employee : public Person
 // // {
-// // private:
-// //     int roll_number, fees;
-
-// // public:
-// //     Student(string name, int age, int roll_number, int fees)
+// //     protected:
+// //     int salary;
+// //     public:
+// //     void Salary()
 // //     {
-// //         this->name = name;
-// //         this->age = age;
-// //         this->roll_number = roll_number;
-// //         this->fees = fees;
+// //         cout<<"My salary is: "<<salary<<endl;
 // //     }
-// //     // void display ()
-// //     // {
-// //     //     cout<<"Name: "<<name<<"\nage: "<<age<<"\nRoll No: "<<roll_number<<"\nFees: "<<fees<<endl;
-// //     // }
+// // };
+
+// // class Manager : public Employee
+// // {
+// //     protected: 
+// //     string department;
+// //     public:
+// //     Manager(string name, int salary, string department)
+// //     {
+// //         this ->name = name;
+// //         this ->salary = salary;
+// //         this ->department = department;
+// //     }
+// //     void display ()
+// //     {
+// //         cout<<"Name: "<<name<<endl;
+// //         cout<<"Salary: "<<salary<<endl;
+// //         cout<<"Department: "<<department<<endl;
+// //     }
 // // };
 
 // // int main ()
 // // {
-// //     Student s("Huzaifa", 21, 1882, 40000);
-// //     s.display();
-// //     // s.work();
-// //     return 0;
+// //     Manager a("Huzaifa", 70000, "SE");
+// //     a.display();
+// //     a.introduce();
+// //     a.Salary();
+// //     // a.name = "Ghufran"; //cant access it bcz of protected access modifier
 // // }
 
 
-// //multilevel inheritance
+// //multiple inheritance
 
 // #include<iostream>
 // using namespace std;
@@ -60,103 +116,104 @@
 //     protected:
 //     string name;
 //     public:
-//     void introduce()
+//     void introduce ()
 //     {
-//         cout<<"My name is: "<<name<<endl;
+//         cout<<"Name: "<<name<<endl;
 //     }
 // };
 
-// class Employee : public Person
+// class Student
 // {
 //     protected:
-//     int salary;
+//     string degree;
 //     public:
-//     void Salary()
-//     {
-//         cout<<"My salary is: "<<salary<<endl;
-//     }
+//     void Subject () {
+//         cout<<"Degree: "<<degree<<endl;
+//     }   
 // };
 
-// class Manager : public Employee
+// class Fees : public Person, Student
 // {
-//     protected: 
-//     string department;
+//     protected:
+//     int fees;
 //     public:
-//     Manager(string name, int salary, string department)
+//     Fees(string name, string degree, int fees)
 //     {
-//         this ->name = name;
-//         this ->salary = salary;
-//         this ->department = department;
+//         this -> name = name;
+//         this -> degree = degree;
+//         this -> fees = fees;
+//     }
+//     void FeesStructure ()
+//     {
+//         cout<<"Fees: "<<fees<<endl;
 //     }
 //     void display ()
 //     {
-//         cout<<"Name: "<<name<<endl;
-//         cout<<"Salary: "<<salary<<endl;
-//         cout<<"Department: "<<department<<endl;
+//         introduce();
+//         Subject();
+//         FeesStructure();
 //     }
 // };
 
 // int main ()
 // {
-//     Manager a("Huzaifa", 70000, "SE");
-//     a.display();
-//     a.introduce();
-//     a.Salary();
-//     // a.name = "Ghufran"; //cant access it bcz of protected access modifier
+//     Fees f("Huzaifa", "CS", 50000);
+//     f.display();
 // }
 
 
-//multiple inheritance
+
+//hierarchical inheritance
 
 #include<iostream>
 using namespace std;
 
-class Person 
-{
+class Human {
     protected:
     string name;
+    int age;
     public:
-    void introduce ()
-    {
-        cout<<"Name: "<<name<<endl;
-    }
+    Human (){}
 };
 
-class Student
+class Student : public Human
 {
     protected:
-    string degree;
+    int marks;
     public:
-    void Subject () {
-        cout<<"Degree: "<<degree<<endl;
-    }   
-};
-
-class Fees : public Person, Student
-{
-    protected:
-    int fees;
-    public:
-    Fees(string name, string degree, int fees)
+    Student(string name, int age, int marks)
     {
         this -> name = name;
-        this -> degree = degree;
-        this -> fees = fees;
+        this -> age = age;
+        this -> marks = marks;
     }
-    void FeesStructure ()
+    void display()
     {
-        cout<<"Fees: "<<fees<<endl;
+        cout<<name<<" "<<age<<" "<<marks<<endl;
     }
-    void display ()
+};
+
+class Teacher : public Human
+{
+    protected:
+    int subject;
+    public:
+    Teacher(string name, int age, int subject)
     {
-        introduce();
-        Subject();
-        FeesStructure();
+        this -> name = name;
+        this -> age = age;
+        this -> subject = subject;
+    }
+    void display()
+    {
+        cout<<name<<" "<<age<<" "<<subject<<endl;
     }
 };
 
 int main ()
 {
-    Fees f("Huzaifa", "CS", 50000);
-    f.display();
+   Student s("Huzaifa", 21, 1012); 
+   Teacher t("Ameer", 29, 1016); 
+   s.display();
+   t.display();
 }
